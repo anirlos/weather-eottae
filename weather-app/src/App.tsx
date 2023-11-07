04c56f48850e4c9d5f24865e7b6e2706544fe12e
aarreumii <140951595+aareumii@users.noreply.github.com>
2023-11-07 19:49:10 +0900

    feat : nav 및 header 작업 완료
    npm install styled-components
    npm install react-router-dom
    npm install @reduxjs/toolkit react-redux
    npm install styled-reset
    설치
    
    newpost, editpost 폴더 생성
    store.js 파일 생성
    GlobalStyles 파일 생성

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainLayout from './components/main/MainLayout';
+import Layout from './components/layout/Layout';
+import NewPost from './pages/newpost/NewPost';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<Layout>
+								<MainLayout />
+							</Layout>
+						}
+					/>
+					<Route
+						path="/archive"
+						element={
+							<Layout>
+								<NewPost />
+							</Layout>
+						}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
+};
 
 export default App;
---
+++ weather-app/src/pages/newpost/NewPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../../components/main/MainLayout';
+
+const NewPost = () => {
+	return (
+		<Main>
+			<div>게시물등록</div>
+		</Main>
+	);
+};
+
+export default NewPost;
