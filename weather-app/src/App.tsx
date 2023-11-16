279c51b3b043477af7bfd6cd27c7d9e2b916d863
aw0605 <101087917+aw0605@users.noreply.github.com>
2023-11-17 01:39:37 +0900

    feat: MSW연결 및 loading, NotFound 파일 생성
    npm install msw --save-dev
    npx msw init ./public --save
    mocks폴더(browser.js, handlers.js)생성
    
    loading.tsx컴포넌트 생성
    NouFound.tsx컴포넌트 생성 및 페이지 연결
    
    Layout.jsx -> Layout.tsx로 확장자 변경

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,42 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainLayout from './components/main/MainLayout';
-import Layout from './components/layout/Layout';
-import NewPost from './pages/newpost/NewPost';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/main/MainLayout";
+import Layout from "./components/layout/Layout";
+import NewPost from "./pages/newpost/NewPost";
+import NotFound from "./pages/not-found/NotFound";
 
 const App: React.FC = () => {
-	return (
-		<div className="App">
-			<Router>
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<Layout>
-								<MainLayout />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/archive"
-						element={
-							<Layout>
-								<NewPost />
-							</Layout>
-						}
-					/>
-					<Route
-						path="/editpost"
-						element={
-							<Layout>
-								<NewPost />
-							</Layout>
-						}
-					/>
-				</Routes>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <MainLayout />
+              </Layout>
+            }
+          />
+          <Route
+            path="/archive"
+            element={
+              <Layout>
+                <NewPost />
+              </Layout>
+            }
+          />
+          <Route
+            path="/editpost"
+            element={
+              <Layout>
+                <NewPost />
+              </Layout>
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- weather-app/src/index.tsx
+++ weather-app/src/index.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './redux/store/store';
-import { Provider } from 'react-redux';
-import { GlobalStyle } from './styles/GlobalStyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./redux/store/store";
+import { Provider } from "react-redux";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+// mocking API 부분
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-	// <React.StrictMode>
-	//   <Root>
-	//     <App />
-	//   </Root>
-	// </React.StrictMode>
-	<Provider store={store}>
-		<GlobalStyle />
+  // <React.StrictMode>
+  //   <Root>
+  //     <App />
+  //   </Root>
+  // </React.StrictMode>
+  <Provider store={store}>
+    <GlobalStyle />
 
-		<App />
-	</Provider>
+    <App />
+  </Provider>
 );
 
 reportWebVitals();
