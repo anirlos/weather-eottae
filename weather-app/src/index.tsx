6518779affbcaf2a845b6058cb04c654c8f0e168
Bomin Kim <viaemma89@gmail.com>
2023-11-25 08:37:53 +0900

    npm install react-daum-postcode 설치 및 api통신

--- weather-app/src/index.tsx
+++ weather-app/src/index.tsx
@@ -8,10 +8,10 @@ import { Provider } from "react-redux";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 // mocking API 부분
-//if (process.env.NODE_ENV === "development") {
-//  const { worker } = require("./mocks/browser");
-//  worker.start();
-//}
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 
 const root = ReactDOM.createRoot(
