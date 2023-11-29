19229bc836d4cfdff46a970b03754f9127c843a6
aarreumii <140951595+aareumii@users.noreply.github.com>
2023-11-29 23:28:14 +0900

    chore: 게시물 위치 온도 api 수정

--- weather-app/src/components/weather/Weather.jsx
+++ weather-app/src/components/weather/Weather.jsx
@@ -1,122 +1,115 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const WeatherComponent = ({ onWeatherUpdate }) => {
-	const [location, setLocation] = useState({
-		loaded: false,
-		coordinates: { lat: '', lng: '' },
-		state: '',
-		temperature: '',
+const WeatherInfo = () => {
+	const [weatherData, setWeatherData] = useState({
+		locationName: '',
+		currentTemp: null,
+		weatherDescription: '',
+		minTemp: null,
+		maxTemp: null,
+		precipitation: null,
+		uvIndex: null,
 	});
+	const [isLoading, setIsLoading] = useState(true);
 
-	const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY; // Google Maps API 키
-	const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY; // 한국 기상청 API 키
+	const OPEN_WEATHER_MAP_API_KEY = 'dcd4cc7754eeecc0a0b7ba1260ac6f25';
+	const WEATHER_API_ENDPOINT =
+		'https://api.openweathermap.org/data/2.5/weather';
+	const ONE_CALL_API_ENDPOINT =
+		'https://api.openweathermap.org/data/2.5/onecall';
 
-	const getState = async (latitude, longitude) => {
-		try {
-			const response = await fetch(
-				`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleMapsApiKey}`
-			);
-			const data = await response.json();
-
-			// "시/도" 정보 추출
-			const addressComponents = data.results[1].address_components;
-			let state = '';
-
-			for (const component of addressComponents) {
-				if (component.types.includes('administrative_area_level_1')) {
-					state = component.long_name;
-				}
-			}
-
-			return state;
-		} catch (error) {
-			console.error('Error fetching state:', error);
-			return '시/도를 불러오는데 실패했습니다';
-		}
-	};
-
-	const getWeatherInfo = async (latitude, longitude) => {
-		try {
-			// 현재 날짜와 시간 구하기
-			const today = new Date();
-			const year = today.getFullYear();
-			const month = (today.getMonth() + 1).toString().padStart(2, '0');
-			const day = today.getDate().toString().padStart(2, '0');
-
-			// API 요청을 위한 시간 설정 (현재 시간보다 이전 시간을 기준으로 설정)
-			let hour = today.getHours();
-			if (hour < 2) {
-				// 00시 기준으로 데이터 요청
-				hour = '00';
+	const getCurrentLocation = () => {
+		return new Promise((resolve, reject) => {
+			if ('geolocation' in navigator) {
+				navigator.geolocation.getCurrentPosition(
+					(position) => {
+						resolve({
+							lat: position.coords.latitude,
+							lon: position.coords.longitude,
+						});
+					},
+					(error) => {
+						reject(error);
+					}
+				);
 			} else {
-				// 이전 시간으로 조정 (예: 11시일 경우 10시 데이터 요청)
-				hour = (Math.floor(hour / 3) * 3 - 3).toString().padStart(2, '0');
+				reject(new Error('Geolocation is not supported by this browser.'));
 			}
-
-			// API 요청을 보내기 위한 URL 생성
-			const apiUrl = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${weatherApiKey}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${year}${month}${day}&base_time=${hour}00&nx=${latitude}&ny=${longitude}`;
-
-			// API 요청 보내기
-			const response = await axios.get(apiUrl);
-
-			// API 응답에서 온도 정보 추출
-			const items = response.data.response.body.items.item;
-			const temperatureItem = items.find((item) => item.category === 'T1H'); // 온도 정보 카테고리
-			const temperature = temperatureItem ? temperatureItem.obsrValue : -9999.0;
-
-			return temperature;
-		} catch (error) {
-			console.error('Error fetching weather info:', error);
-			return -9999.0;
-		}
-	};
-
-	const onSuccess = async (position) => {
-		const { latitude, longitude } = position.coords;
-		const state = await getState(latitude, longitude);
-		const temperature = await getWeatherInfo(latitude, longitude);
-
-		setLocation({
-			loaded: true,
-			coordinates: { lat: latitude, lng: longitude },
-			state,
-			temperature,
 		});
-		onWeatherUpdate(temperature);
 	};
 
-	const onError = (error) => {
-		setLocation({ loaded: true, error });
-		onWeatherUpdate(-9999.0); // 에러 발생 시 -9999.0 전달
+	const fetchWeatherData = async (lat, lon) => {
+		setIsLoading(true);
+		try {
+			const response = await axios.get(WEATHER_API_ENDPOINT, {
+				params: {
+					lat: lat,
+					lon: lon,
+					appid: OPEN_WEATHER_MAP_API_KEY,
+					units: 'metric',
+					lang: 'kr',
+				},
+			});
+
+			const locationName = response.data.name;
+			const currentTemp = response.data.main.temp;
+			const weatherDescription = response.data.weather[0].description;
+
+			const oneCallResponse = await axios.get(ONE_CALL_API_ENDPOINT, {
+				params: {
+					lat: lat,
+					lon: lon,
+					exclude: 'current,minutely,hourly,alerts',
+					appid: OPEN_WEATHER_MAP_API_KEY,
+					units: 'metric',
+					lang: 'kr',
+				},
+			});
+
+			const dailyData = oneCallResponse.data.daily[0];
+
+			setWeatherData({
+				locationName,
+				currentTemp,
+				weatherDescription,
+				minTemp: dailyData.temp.min,
+				maxTemp: dailyData.temp.max,
+				precipitation: dailyData.rain ? dailyData.rain['1h'] : 0,
+				uvIndex: dailyData.uvi,
+			});
+		} catch (error) {
+			console.error('Error fetching weather data:', error);
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
 	useEffect(() => {
-		if (!('geolocation' in navigator)) {
-			onError({
-				code: 0,
-				message: 'Geolocation not supported',
-			});
-		}
+		const loadWeatherData = async () => {
+			try {
+				const { lat, lon } = await getCurrentLocation();
+				await fetchWeatherData(lat, lon);
+			} catch (error) {
+				console.error('Error getting user location:', error);
+				setIsLoading(false);
+			}
+		};
 
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		loadWeatherData();
 	}, []);
+
+	if (isLoading) {
+		return <div>날씨 데이터를 불러오는 중입니다...</div>;
+	}
 
 	return (
 		<div>
-			{location.loaded ? (
-				<div>
-					<p>
-						{location.temperature !== -9999.0
-							? `${location.temperature}°C`
-							: '날씨 정보 없음'}
-					</p>
-				</div>
-			) : (
-				<div>Loading...</div>
-			)}
+			<p>
+				{weatherData.currentTemp ? weatherData.currentTemp.toFixed(1) : 'N/A'}°C
+			</p>
 		</div>
 	);
 };
 
-export default WeatherComponent;
+export default WeatherInfo;
