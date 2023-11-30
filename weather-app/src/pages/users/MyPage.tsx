6be9d78fcdc5535372e30ccf44803f36114a31d6
Mia7351 <whitneyk735@gmail.com>
2023-11-30 15:58:56 +0900

    회원정보페이지 수정 1차

--- weather-app/src/pages/users/MyPage.tsx
+++ weather-app/src/pages/users/MyPage.tsx
@@ -36,8 +36,30 @@ const MyPage = () => {
   };
 
   const [profileImage, setProfileImage] = useState("/person-circle.svg");
-  const [nickname, setNickname] = useState("NickName");
+  const [nickName, setNickName] = useState("NickName");
   const [statusMessage, setStatusMessage] = useState("Have a nice day!");
+
+  //백엔드에서 nickname, message불러오기
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // 로컬 스토리지에서 access_token 가져오기
+        const token = localStorage.getItem("access_token");
+        const response = await axios.get("http://43.200.188.52:8080/api/user", {
+          headers: {
+            Authorization: token, // 토큰을 요청 헤더에 추가
+          },
+        });
+        setNickName(response.data.nickName || "nickName"); // 백엔드 응답이 없을 경우 기본값 사용
+        setStatusMessage(response.data.message || "Have a nice day!"); // 백엔드 응답이 없을 경우 기본값 사용
+        setProfileImage(response.data.imageUrl || "/person-circle.svg");
+      } catch (error) {
+        console.error("백엔드로부터 데이터를 불러오는 데 실패했습니다.", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleUserInfoChange = (
     newProfileImage: string,
@@ -45,7 +67,7 @@ const MyPage = () => {
     newStatusMessage: string
   ) => {
     setProfileImage(newProfileImage);
-    setNickname(newNickname);
+    setNickName(newNickname);
     setStatusMessage(newStatusMessage);
     <UserInfo
       isOpen={isUserInfoModalOpen}
@@ -56,10 +78,10 @@ const MyPage = () => {
 
   return (
     <MyPageContainer>
-      <ProfileImage src="/person-circle.svg" alt="프로필 이미지" />
-
+      {/* <ProfileImage src="/person-circle.svg" alt="프로필 이미지" /> */}
+      <ProfileImage src={profileImage} alt="프로필 이미지" />
       <ProfileInfo>
-        <Nickname>{nickname} 님</Nickname>
+        <Nickname>{nickName} 님</Nickname>
         {/* <p>현재 위치 : 서울특별시 중구</p> */}
         <StatusMessage>{statusMessage}</StatusMessage>
       </ProfileInfo>
