96095208b762400d590d809acc0857b151be472d
justkeepgoin <manage06@naver.com>
2024-01-16 16:53:19 +0900

    fix :  예외처리 제거
    - 소켓 연결이 안될 경우 전체 페이지에서 에러 알림 발생
    - 소켓 연결 실패시 예외처리 제거

--- weather-app/src/api/socket.js
+++ weather-app/src/api/socket.js
@@ -5,13 +5,4 @@ const socket = io(process.env.REACT_APP_SOCKET_URL, {
   transports: ["websocket"], // 웹소켓 연결만 사용하도록 설정
 });
 
-socket.on("disconnect", () => {
-  alert("소켓 연결이 끊어졌습니다. 다시 연결해주세요.");
-});
-
-socket.on("connect_error", (error) => {
-  console.error("소켓 연결 실패:", error);
-  alert("소켓 연결에 실패했습니다. 다시 시도해주세요.");
-});
-
 export default socket;
