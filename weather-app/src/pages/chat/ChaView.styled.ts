e7fa682cd6722bf30afc2dc87b0705196877ab0f
justkeepgoin <manage06@naver.com>
2024-01-17 18:09:23 +0900

    fix : 빌드를 위한 에러 수정
    - git이 대소문자를 구분할 수 있도록 설정

---
+++ weather-app/src/pages/chat/ChaView.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ChatViewContainer = styled.div`
+  padding: 1rem;
+  height: 730px;
+  margin: 3rem;
+  user-select: none; // 드래그 선택 방지
+  }
+`;
