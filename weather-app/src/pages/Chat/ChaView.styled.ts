b7a2e463afe8199b47310df27a15d63785421884
aarreumii <140951595+aareumii@users.noreply.github.com>
2024-01-17 20:28:14 +0900

    fix : 괄호 삭제

--- weather-app/src/pages/Chat/ChaView.styled.ts
+++ weather-app/src/pages/Chat/ChaView.styled.ts
@@ -1,9 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ChatViewContainer = styled.div`
-  padding: 1rem;
-  height: 730px;
-  margin: 3rem;
-  user-select: none; // 드래그 선택 방지
-  }
+	padding: 1rem;
+	height: 730px;
+	margin: 3rem;
+	user-select: none; // 드래그 선택 방지
 `;
